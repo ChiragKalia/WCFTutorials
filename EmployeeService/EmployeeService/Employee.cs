43a2833dc060635a931d3f8f27bdb598784c38d0
chiragkalia <chirag.kalia@valiancesolutions.com>
2017-04-20 18:14:01 +0530

    Added EmployeeService

---
+++ EmployeeService/EmployeeService/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService
+{ 
+    public class Employee
+    {
+
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        private string _gender;
+        public string gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        private DateTime _dateOfBirth;
+        public DateTime dateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+    }
+}
---
+++ EmployeeService/EmployeeService/EmployeeService.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in both code and config file together.
+    public class EmployeeService : IEmployeeService
+    {
+        public Employee GetEmployee(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                Employee employee = entities.spGetEmployee(id).Select(i => new Employee { dateOfBirth = i.DateOfBirth, gender = i.Gender, name = i.Name }).FirstOrDefault();
+                return employee;
+            }
+        }
+
+        public void SaveEmployee(Employee employee)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                entities.spSaveEmployee(employee.name, employee.gender, employee.dateOfBirth);
+                entities.SaveChanges();
+            }
+        }
+    }
+}
